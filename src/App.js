5a99ac88643615d0bdaee705fe9a1018dd00dbb1
Sade Joseph <feojoseph1@gmail.com>
2023-11-20 17:49:04 +0000

    fetch json placeholder

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './Robots';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state= {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users =>this.setState({ robots: users}));
+  }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value})
