e7e242b9dc9bcfe5768b1b884431ea83883e7e8e
Sade Joseph <feojoseph1@gmail.com>
2023-11-20 10:54:26 +0000

    app.js state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './Robots';
 
-const App = () => {
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state= {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+  render() {
     return (
       <div className='tc'>
         <h1>RoboFriends</h1>
         <SearchBox/>
-        <CardList robots={robots}/>
+        <CardList robots={this.state.robots}/>
       </div>
     );
-}
+   }
+  }
+   
+
 
 export default App;
