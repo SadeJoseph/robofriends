00d7239f9922b243c0668d188dd7e884ce99d64a
Sade Joseph <feojoseph1@gmail.com>
2023-11-20 16:30:04 +0000

    app css, font filer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './Robots';
-
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -12,12 +12,22 @@ class App extends Component {
       searchfield: ''
     }
   }
+
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value})
+   }
+
+
   render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
     return (
       <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox/>
-        <CardList robots={this.state.robots}/>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
       </div>
     );
    }
--- src/Robots.js
+++ src/Robots.js
@@ -27,6 +27,36 @@ export const robots = [
         id: 5,
         name: 'Chelsea Dietrich',
         username: 'Kamran',
-        email: 'Sincere@april.biz'
+        email: 'Lucio_Hettinger@annie.ca'
+    } ,
+    {
+        id: 6,
+        name: 'Mrs.Dennis Schulist',
+        username: 'Leopoldo Corkery ',
+        email: 'Karley_Dach@jasper.info'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Rrunolfdottir',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io'
+    } ,
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriag,Stanton',
+        email: 'Rey.Padberg@karina.biz'
     } 
 ]
